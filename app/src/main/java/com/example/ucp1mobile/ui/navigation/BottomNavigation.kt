8c5d373dc02cfbb1674fc13542946c5a451ecc91
hikmah76 <nur.hikmah190504@gmail.com>
2024-12-24 08:47:14 +0700

    Membuat  bar navigasi di bawah aplikasi

---
+++ app/src/main/java/com/example/ucp1mobile/ui/navigation/BottomNavigation.kt
@@ -0,0 +1,52 @@
+package com.example.ucp1mobile.navigation
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+// membuat bar navigasi bawah yang memungkinkan pengguna berpindah antar halaman dosen dan matakuliah
+@Composable
+fun BottomNavigationBar(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        modifier = modifier
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        val navigationItems = listOf(
+            NavigationItem.Dosen,
+            NavigationItem.Matakuliah
+        )
+
+        navigationItems.forEach { item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.title
+                    )
+                },
+                label = { Text(item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
