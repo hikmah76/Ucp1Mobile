d2873af4428308ea94493fa0107635fce905535f
hikmah76 <nur.hikmah190504@gmail.com>
2024-12-24 08:18:38 +0700

    Tabel matakuliah

--- app/src/main/java/com/example/ucp1mobile/data/entity/Matakuliah.kt
+++ app/src/main/java/com/example/ucp1mobile/data/entity/Matakuliah.kt
@@ -2,15 +2,14 @@ package com.example.ucp1mobile.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
-@Entity(tableName = "Matakuliah")
-data class  Matakuliah(
+//Tabel matakuliah
+@Entity(tableName = "matakuliah")
+data class Matakuliah(
     @PrimaryKey
-    val kode: String,
-    val nama: String,
-    val sks: String,
-    val semester: String,
-    val jenis: String,
-    val dosenPengampu: String,
+    val kode: String = "",
+    val nama: String = "",
+    val sks: String = "",
+    val semester: String = "",
+    val jenis: String = "",
+    val dosenPengampu: String = ""
 )
-
