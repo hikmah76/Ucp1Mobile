9c7202c476a889a1c78a6dfafb65f07214580c5b
hikmah76 <nur.hikmah190504@gmail.com>
2024-12-24 08:13:25 +0700

    Untuk mengelola data Matakuliah

--- app/src/main/java/com/example/ucp1mobile/data/dao/MatakuliahDao.kt
+++ app/src/main/java/com/example/ucp1mobile/data/dao/MatakuliahDao.kt
@@ -8,24 +8,20 @@ import androidx.room.Update
 import com.example.ucp1mobile.data.entity.Matakuliah
 import kotlinx.coroutines.flow.Flow
 
-@Dao
+@Dao //Berfungsi untuk mengakses, menyimpan, memperbarui, dan menghapus data Matakuliah di Room Database
 interface MatakuliahDao {
     @Insert
-    suspend fun insertMatakuliah(matakuliah: Matakuliah) //menggunakan suspend karena operasinya berat (insert, update, delete)
+    suspend fun insertMatakuliah(matakuliah: Matakuliah)
 
-    //getAllMahasiswa
     @Query("SELECT * FROM matakuliah ORDER BY nama ASC")
-    fun getAllMatakuliah() : Flow<List<Matakuliah>>
+    fun getAllMatakuliah(): Flow<List<Matakuliah>>
 
-    //getMahasiswa
-    @Query("SELECT * FROM matakuliah WHERE nim = :nim")
-    fun getMatakuliah(nim: String) : Flow<Matakuliah>
+    @Query("SELECT * FROM matakuliah WHERE kode = :kode")
+    fun getMth(kode: String): Flow<Matakuliah>  // Diganti jadi getMth
 
-    //deleteMahasiswa
     @Delete
     suspend fun deleteMatakuliah(matakuliah: Matakuliah)
 
-    //updateMahasiswa
     @Update
     suspend fun updateMatakuliah(matakuliah: Matakuliah)
 }
