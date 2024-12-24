62140bda4d187f87ce70d76bb5dbc9ab54d966cf
hikmah76 <nur.hikmah190504@gmail.com>
2024-12-24 08:17:50 +0700

    Tabel dosen

--- app/src/main/java/com/example/ucp1mobile/data/entity/Dosen.kt
+++ app/src/main/java/com/example/ucp1mobile/data/entity/Dosen.kt
@@ -2,13 +2,12 @@ package com.example.ucp1mobile.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+//Tabel Dosen
 @Entity(tableName = "dosen")
-data class  Dosen(
+data class Dosen(
     @PrimaryKey
-    val nidn: String,
-    val nama: String,
-    val jenisKelamin: String,
-
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = ""
 )
 
