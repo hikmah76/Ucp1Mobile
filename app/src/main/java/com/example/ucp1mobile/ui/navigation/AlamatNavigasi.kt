749b10e6744aa967f93ddcee0bfd175bd9ded0fd
hikmah76 <nur.hikmah190504@gmail.com>
2024-12-24 08:41:54 +0700

    Untuk menentukan alamat atau rute navigasi

--- app/src/main/java/com/example/ucp1mobile/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp1mobile/ui/navigation/AlamatNavigasi.kt
@@ -1,21 +1,45 @@
 package com.example.ucp1mobile.ui.navigation
-
+// untuk menentukan alamat atau rute navigasi, seperti halaman untuk dosen dan matakuliah. Dengan  parameter seperti NIDN dan KODE
 interface AlamatNavigasi {
     val route: String
 }
-
-object DestinasiHome : AlamatNavigasi {
-    override val route = "home"
+object DestinasiDosenHome {
+    const val route = "home_dosen"
 }
 
-object DestinasiDetail : AlamatNavigasi {
-    override val route = "detail"
-    const val NIM = "nim"
-    val routesWithArg = "$route/{$NIM}"
+object DestinasiDosenInsert {
+    const val route = "insert_dosen"
 }
 
-object DestinasiUpdate : AlamatNavigasi {
-    override val route = "update"
-    const val NIM = "nim"
-    val routesWithArg = "$route/{$NIM}"
+object DestinasiDosenDetail {
+    const val route = "detail_dosen"
+    const val NIDN = "nidn"
+    val routeWithArg = "$route/{$NIDN}"
+}
+
+object DestinasiDosenUpdate {
+    const val route = "update_dosen"
+    const val NIDN = "nidn"
+    val routeWithArg = "$route/{$NIDN}"
+}
+
+// Matakuliah
+object DestinasiMatakuliahHome : AlamatNavigasi {
+    override val route = "home_matakuliah"
+}
+
+object DestinasiMatakuliahInsert : AlamatNavigasi {
+    override val route = "insert_matakuliah"
+}
+
+object DestinasiMatakuliahDetail : AlamatNavigasi {
+    override val route = "detail_matakuliah"
+    const val KODE = "kode"
+    val routeWithArg = "$route/{$KODE}"
+}
+
+object DestinasiMatakuliahUpdate : AlamatNavigasi {
+    override val route = "update_matakuliah"
+    const val KODE = "kode"
+    val routeWithArg = "$route/{$KODE}"
 }
